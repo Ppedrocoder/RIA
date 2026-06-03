9e1a121145d012359361eada3d05364d5a7100e5
Pedro Ricardo dos Santos Gomes <rpedroricardo87@gmail.com>
2026-06-03 16:28:37 +0000

    refactor: change @input to input

--- frontend/atelie/src/app/form-actions/form-actions.ts
+++ frontend/atelie/src/app/form-actions/form-actions.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { output } from '@angular/core';
@@ -8,9 +8,9 @@ import { output } from '@angular/core';
   imports: [CommonModule, ButtonModule],
 template:`
   <div class="card flex justify-center m-4">
-              <p-button [disabled]="formAtelie().invalid()" label="{{ formAtelie.id ? 'Atualizar' : 'Salvar' }}" (click)="onSave()" />
+              <p-button [disabled]="formAtelie().invalid()" label="{{ formAtelie().id ? 'Atualizar' : 'Salvar' }}" (click)="onSave()" />
   </div>
-          @if (formAtelie.id) {
+          @if (formAtelie().id) {
             <div class="card flex justify-center m-4">
             <p-button [disabled]="formAtelie().invalid()" severity="secondary" label="Cancelar" (click)="onResetForm()" />
             </div>
@@ -20,7 +20,7 @@ template:`
   styleUrl: './form-actions.css',
 })
 export class FormActions {
-  @Input() formAtelie: any;
+  formAtelie = input<any>();
   save = output<void>();
   resetForm = output<void>();
 
