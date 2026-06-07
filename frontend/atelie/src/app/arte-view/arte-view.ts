dfd99240f9912db4231fe255db2faf402d4c3fb6
Pedro <rpedroricardo87@gmail.com>
2026-06-07 18:57:16 -0300

    feat: add field set module

--- frontend/atelie/src/app/arte-view/arte-view.ts
+++ frontend/atelie/src/app/arte-view/arte-view.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { DividerModule } from 'primeng/divider';
 import { ArteService } from '../services/service';
 import { inject } from '@angular/core';
-import { RouterModule } from "@angular/router";
+import { ActivatedRoute, RouterModule } from "@angular/router";
+import { FieldsetModule } from 'primeng/fieldset';
 
 @Component({
   selector: 'app-arte-view',
-  imports: [CommonModule, DividerModule, RouterModule],
+  imports: [CommonModule, DividerModule, RouterModule, FieldsetModule],
   template: `
   <p-divider align="center" class="mb-4">
     <h2 class="text-4xl font-bold m-4">Ateliê Potiguar</h2>
@@ -16,10 +17,22 @@ import { RouterModule } from "@angular/router";
     <a routerLink="/" class="pi pi-arrow-left"></a>
   </div>
   <div class="mb-4 p-8 flex items-center justify-center flex-col">
-    <h1 class="justify-content">Arte: {{ arte()?.name }}</h1>
-    <img [src]="arte()?.foto" alt="{{ arte()?.name }}" class="w-full h-auto mb-4" />
-    <p class="mb-4">{{ arte()?.description }}</p>
-    <p class="mb-4">Tipo de Arte: {{ tipoArteNome }}</p>
+    <h1 class="text-3xl font-bold mb-4">{{ arte()?.name }}</h1>
+    <img [src]="arte()?.foto" alt="{{ arte()?.name }}" class="max-h-96 w-auto object-contain mb-4" />
+    <div class="card mb-4">
+            <p-fieldset legend="Descrição">
+                <p class="m-0">
+                    {{ arte()?.description }}
+                </p>
+            </p-fieldset>
+    </div>
+    <div class="card mb-4">
+            <p-fieldset legend="Tipo de Arte">
+                <p class="m-0">
+                    {{ tipoArteNome }}
+                </p>
+            </p-fieldset>
+    </div>
   </div>
   <p-divider align="center" class="mt-4">
     <h2 class="text-2xl font-bold m-4">Feito por: Pedro Ricardo</h2>
@@ -30,9 +43,10 @@ import { RouterModule } from "@angular/router";
 export class ArteView {
   arteService = inject(ArteService);
   artes = this.arteService.getArtes();
-  id = input<number>();
+  private route = inject(ActivatedRoute);
+  id: string | null = this.route.snapshot.paramMap.get('id');
   arte = computed(() => {
-    const id = Number(this.id());
+    const id = Number(this.id);
     return this.arteService.getArteById(id);
   });
   constructor() {
