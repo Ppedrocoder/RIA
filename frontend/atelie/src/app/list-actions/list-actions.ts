038c08a694d9b2796f34b3da2ac5617c894fb5a2
Pedro <rpedroricardo87@gmail.com>
2026-06-07 18:58:48 -0300

    feat: add art view button

--- frontend/atelie/src/app/list-actions/list-actions.ts
+++ frontend/atelie/src/app/list-actions/list-actions.ts
@@ -1,17 +1,22 @@
-import { Component, output } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { input } from '@angular/core';
 import { Arte } from '../app';
+import { Router, RouterModule } from "@angular/router";
+
 
 @Component({
   selector: 'app-list-actions',
-  imports: [CommonModule, ButtonModule],
+  imports: [CommonModule, ButtonModule, RouterModule],
   template: `
     <div class="flex gap-4 mt-1">
-        <p-button label="Editar" [outlined]="true" class="w-full" styleClass="w-full" (click)="onEdit(a())" />
+        <div class="flex gap-6 justify-center w-full">
         <p-button label="Excluir" severity="secondary" [outlined]="true" class="w-full" styleClass="w-full" (click)="onDelete(a().id)" />
-    </div> 
+        <p-button label="Visualizar" severity="contrast" [outlined]="true" class="w-full" styleClass="w-full" (click)="onView(a().id)" />
+        <p-button label="Editar" [outlined]="true" class="w-full" styleClass="w-full" (click)="onEdit(a())" />
+        </div>
+    </div>
   ` ,
   styleUrl: './list-actions.css',
 })
@@ -28,5 +33,10 @@ export class ListActions {
     if (!id) return;
 
     this.delete.emit(id);
+  }
+  router = inject(Router);
+
+  onView(id?: number) {
+    this.router.navigate(['', id]);
   }
 }
